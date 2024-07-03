5d93f3da011d00a07f64b83ae012be7f573fa9ce
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-07-03 02:36:29 +0200

    add clang lsp

--- init.lua
+++ init.lua
@@ -596,10 +596,10 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
         -- gopls = {},
-        -- TOMAS: Python lsp?
+        -- TOMAS: Python lsp? Also, clang for C / C++ I think Also, clang for C / C++ I think
         pyright = {},
+        clangd = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
