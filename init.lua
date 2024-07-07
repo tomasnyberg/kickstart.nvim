8d162215c5250395d1cccebaefb69b0876ab50f0
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-07-07 16:46:05 +0200

    add dap

--- init.lua
+++ init.lua
@@ -308,6 +308,93 @@ require('lazy').setup({
       })
     end,
   },
+  -- Tomas: Debugging!
+  {
+    'mfussenegger/nvim-dap',
+    dependencies = {
+      'rcarriga/nvim-dap-ui',
+      'theHamsta/nvim-dap-virtual-text',
+      'nvim-neotest/nvim-nio',
+      'williamboman/mason.nvim',
+    },
+    config = function()
+      local dap = require 'dap'
+      local ui = require 'dapui'
+
+      require('dapui').setup()
+      dap.adapters.lldb = {
+        type = 'executable',
+        -- TOMAS NOTE: (!!!)
+        -- To make this work you will need the lldb-dap-18 thing. you can install it like this.
+        -- sudo bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)" # Instructions from https://apt.llvm.org/
+        -- cd $(dirname $(which lldb-17))
+        -- sudo ln -s lldb-vscode-17 lldb-vscode
+        command = '/usr/bin/lldb-dap-18', -- adjust as needed, must be absolute path
+        name = 'lldb',
+      }
+      dap.configurations.c = {
+        {
+          name = 'Launch',
+          type = 'lldb',
+          request = 'launch',
+          program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+          end,
+          cwd = '${workspaceFolder}',
+          stopOnEntry = true,
+          args = {},
+        },
+      }
+
+      -- local elixir_ls_debugger = vim.fn.exepath 'elixir-ls-debugger'
+      -- if elixir_ls_debugger ~= '' then
+      --   dap.adapters.mix_task = {
+      --     type = 'executable',
+      --     command = elixir_ls_debugger,
+      --   }
+      --
+      --   dap.configurations.elixir = {
+      --     {
+      --       type = 'mix_task',
+      --       name = 'phoenix server',
+      --       task = 'phx.server',
+      --       request = 'launch',
+      --       projectDir = '${workspaceFolder}',
+      --       exitAfterTaskReturns = false,
+      --       debugAutoInterpretAllModules = false,
+      --     },
+      --   }
+      -- end
+
+      vim.keymap.set('n', '<space>b', dap.toggle_breakpoint)
+      vim.keymap.set('n', '<space>gb', dap.run_to_cursor)
+
+      -- Eval var under cursor
+      vim.keymap.set('n', '<space>?', function()
+        require('dapui').eval(nil, { enter = true })
+      end)
+
+      vim.keymap.set('n', '<F1>', dap.continue)
+      vim.keymap.set('n', '<F2>', dap.step_into)
+      vim.keymap.set('n', '<F3>', dap.step_over)
+      vim.keymap.set('n', '<F4>', dap.step_out)
+      vim.keymap.set('n', '<F5>', dap.step_back)
+      vim.keymap.set('n', '<F13>', dap.restart)
+
+      dap.listeners.before.attach.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        ui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        ui.close()
+      end
+    end,
+  },
   -- END TOMAS new packages
 
   -- NOTE: Plugins can also be added by using a table,
