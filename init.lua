b7959f3b0e64d8681b1a38d7407432b391151925
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-08-03 14:07:30 +0200

    add clojure lsp

--- init.lua
+++ init.lua
@@ -758,6 +758,8 @@ require('lazy').setup({
             command = 'clippy',
           },
         },
+
+        clojure_lsp = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
