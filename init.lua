eb8b24d902ba3025660c3e3689697b7473a8397e
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-10-12 09:03:33 +0200

    make <leader>s/ search across all files rather than just the open ones

--- init.lua
+++ init.lua
@@ -687,8 +687,8 @@ require('lazy').setup({
       --  See `:help telescope.builtin.live_grep()` for information about particular keys
       vim.keymap.set('n', '<leader>s/', function()
         builtin.live_grep {
-          grep_open_files = true,
-          prompt_title = 'Live Grep in Open Files',
+          -- grep_open_files = true, -- TOMAS: commented this out so that it searches all files
+          prompt_title = 'Live Grep Over Project Files',
         }
       end, { desc = '[S]earch [/] in Open Files' })
 
